a059c33d7abed8616cca8ae18c56790ee3b4b500
Robinson De La Cruz <robinson.delacruz@vasscompany.com>
2025-03-17 15:37:41 -0500

    feat: add environment config with Joi validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(envs.port, () =>
+    console.log(`API running on port: ${envs.port}`),
+  );
 }
 bootstrap();
